2de4ad8c526d030277fcc28f59c6166d7da43427
Lalu <Lalu@Lalu>
2019-01-02 09:05:16 +0530

    zipkin

---
+++ src/main/java/com/spring/zipkin/ZipkinApplication.java
@@ -0,0 +1,18 @@
+package com.spring.zipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import zipkin.server.EnableZipkinServer;
+
+
+@SpringBootApplication
+@EnableZipkinServer
+public class ZipkinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZipkinApplication.class, args);
+	}
+
+}
+
